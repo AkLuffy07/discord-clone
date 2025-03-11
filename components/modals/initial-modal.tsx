515865786b1d9743ffc5192ecffb61ca2a2b5ea6
Akhil <akhilsringeri123@gmail.com>
2025-03-11 19:16:36 +0530

    added uploadthing configuration for image upload

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -24,6 +24,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
+import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -76,7 +77,21 @@ export const InitialModal = () => {
                     className="space-y-8" >
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Image Upload
+                                <FormField 
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                             </div>
 
                             <FormField 
@@ -102,7 +117,7 @@ export const InitialModal = () => {
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4">
                                 <Button disabled={isLoading}
-                                variant="primary" >
+                                variant="primary" type="submit" >
                                     Create
                                 </Button>
                         </DialogFooter>
